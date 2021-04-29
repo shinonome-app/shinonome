1e697fa058a10c496606b4ee2248d4c4dada4397
takahashim <takahashimm@gmail.com>
2021-04-29 20:42:51 +0900

    add base_person

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :base_people
   resources :news
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
