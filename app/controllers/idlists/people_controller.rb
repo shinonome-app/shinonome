73266f9950d92068493c18ef4afb4989fc863634
takahashim <takahashimm@gmail.com>
2021-05-04 02:44:50 +0900

    list idlists

--- app/controllers/idlists/people_controller.rb
+++ app/controllers/idlists/people_controller.rb
@@ -1,4 +1,6 @@
 class Idlists::PeopleController < ApplicationController
   def index
+    key = params[:key][0] + "%"
+    @people = Person.where("last_name_kana like ?", key).order(:sortkey, :last_name_kana, :sortkey2, :first_name_kana, :id).all
   end
 end
--- app/controllers/idlists/workers_controller.rb
+++ app/controllers/idlists/workers_controller.rb
@@ -1,4 +1,6 @@
 class Idlists::WorkersController < ApplicationController
   def index
+    key = params[:key][0] + "%"
+    @workers = Worker.where("name_kana like ?", key).order(:sortkey, :name_kana, :name, :id).all
   end
 end
