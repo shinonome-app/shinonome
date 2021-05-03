0efb13d7dc332c3da49c6ad366528a68f08522d5
takahashim <takahashimm@gmail.com>
2021-05-03 17:08:39 +0900

    update books

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -1,62 +1,65 @@
 # frozen_string_literal: true
 
-class BooksController < ApplicationController
-  before_action :set_book, only: %i[show edit update destroy]
+module Admin
+  # 作品管理
+  class BooksController < Admin::ApplicationController
+    before_action :set_book, only: %i[show edit update destroy]
 
-  # GET /books
-  def index
-    @books = Book.all
-  end
-
-  # GET /books/1
-  def show; end
-
-  # GET /books/new
-  def new
-    @book = Book.new
-  end
-
-  # GET /books/1/edit
-  def edit; end
-
-  # POST /books
-  def create
-    @book = Book.new(book_params)
-
-    if @book.save
-      redirect_to @book, notice: 'Book was successfully created.'
-    else
-      render :new
+    # GET /books
+    def index
+      @books = Book.all
     end
-  end
 
-  # PATCH/PUT /books/1
-  def update
-    if @book.update(book_params)
-      redirect_to @book, notice: 'Book was successfully updated.'
-    else
-      render :edit
+    # GET /books/1
+    def show; end
+
+    # GET /books/new
+    def new
+      @book = Book.new
     end
-  end
 
-  # DELETE /books/1
-  def destroy
-    @book.destroy
-    redirect_to books_url, notice: 'Book was successfully destroyed.'
-  end
+    # GET /books/1/edit
+    def edit; end
 
-  private
+    # POST /books
+    def create
+      @book = Book.new(book_params)
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_book
-    @book = Book.find(params[:id])
-  end
+      if @book.save
+        redirect_to [:admin, @book], notice: 'Book was successfully created.'
+      else
+        render :new
+      end
+    end
 
-  # Only allow a list of trusted parameters through.
-  def book_params
-    params.require(:book).permit(:title, :title_kana, :subtitle, :subtitle_kana, :collection, :collection_kana,
-                                 :orig_title, :kana_type_id, :author_display_name, :first_appearance, :description,
-                                 :description_person_id, :status, :started_on, :copyright_flag, :note, :orig_text,
-                                 :updated_at, :user_id, :update_flag, :sortkey)
+    # PATCH/PUT /books/1
+    def update
+      if @book.update(book_params)
+        redirect_to [:admin, @book], notice: 'Book was successfully updated.'
+      else
+        render :edit
+      end
+    end
+
+    # DELETE /books/1
+    def destroy
+      @book.destroy
+      redirect_to admin_books_url, notice: 'Book was successfully destroyed.'
+    end
+
+    private
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_book
+      @book = Book.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def book_params
+      params.require(:book).permit(:title, :title_kana, :subtitle, :subtitle_kana, :collection, :collection_kana,
+                                   :orig_title, :kana_type_id, :author_display_name, :first_appearance, :description,
+                                   :description_person_id, :status, :started_on, :copyright_flag, :note, :orig_text,
+                                   :updated_at, :user_id, :update_flag, :sortkey)
+    end
   end
 end
