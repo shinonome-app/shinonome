b74e095a7eb277a493c4c6836df7b8abe39e9b56
takahashim <takahashimm@gmail.com>
2021-05-04 11:07:17 +0900

    fix count

--- app/controllers/idlists/people_controller.rb
+++ app/controllers/idlists/people_controller.rb
@@ -1,6 +1,8 @@
 class Idlists::PeopleController < ApplicationController
   def index
     key = params[:key][0] + "%"
-    @people = Person.where("last_name_kana like ?", key).order(:sortkey, :last_name_kana, :sortkey2, :first_name_kana, :id).all
+    people = Person.where("last_name_kana like ?", key).order(:sortkey, :last_name_kana, :sortkey2, :first_name_kana, :id)
+    @count = people.count
+    @people = people.all
   end
 end
--- app/controllers/idlists/workers_controller.rb
+++ app/controllers/idlists/workers_controller.rb
@@ -1,6 +1,8 @@
 class Idlists::WorkersController < ApplicationController
   def index
     key = params[:key][0] + "%"
-    @workers = Worker.where("name_kana like ?", key).order(:sortkey, :name_kana, :name, :id).all
+    workers = Worker.where("name_kana like ?", key).order(:sortkey, :name_kana, :name, :id)
+    @count = workers.count
+    @workers = workers.all
   end
 end
