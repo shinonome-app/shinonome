a88022db71285662abfa11e06ae9a95c47b6730c
takahashim <takahashimm@gmail.com>
2021-05-06 01:57:32 +0900

    fix seeds

---
+++ db/seeds/news.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+News.connection.execute('TRUNCATE TABLE news;')
+
+news = (1..200).map do
+  publish_date = Faker::Date.between(from: '1998-01-01', to: '2021-05-01')
+  title = Faker::Lorem.sentence(word_count: 5, random_words_to_add: 10).chop
+  body = ''.dup
+  (3..30).to_a.sample.times do
+    body << Faker::Lorem.sentence(word_count: 20, random_words_to_add: 30)
+  end
+
+  body.gsub(/。/) { rand(100) > 70 ? "。\n" : "。\n\n" }
+
+  {
+    body: body,
+    flag: rand(100) > 80,
+    published_on: publish_date,
+    title: title,
+    created_at: Time.current,
+    updated_at: Time.current
+  }
+end
+
+sorted_news = news.sort_by { |x| x[:published_on] }
+
+News.insert_all(sorted_news)
--- db/seeds/people.rb
+++ db/seeds/people.rb
@@ -7,7 +7,10 @@ people = (1..2000).map do |n|
   died_year = born_date.year + 30 + rand(30)
   died_date = Faker::Date.in_date_period(year: died_year)
   desc = ''.dup
-  (3..8).to_a.sample.times { desc << Faker::Lorem.sentence(word_count: 10, random_words_to_add: 15) }
+  (3..8).to_a.sample.times do
+    desc << Faker::Lorem.sentence(word_count: 10, random_words_to_add: 15)
+  end
+
   {
     last_name: name.last.kanji,
     last_name_kana: name.last.hiragana,
