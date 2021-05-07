5232310cc71208aa08c5e996c2e4df1e92d0f7a9
takahashim <takahashimm@gmail.com>
2021-05-07 12:23:54 +0900

    fix books seeds

--- db/seeds/books.rb
+++ db/seeds/books.rb
@@ -8,13 +8,20 @@ person_id_list = Person.all.pluck(:id)
 book_status_id_list = BookStatus.all.pluck(:id)
 user_id_list = User.all.pluck(:id)
 
+FIRST_CHAR = 'あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん★'
+
 books = (1..5000).map do |n|
   desc = ''.dup
   (3..8).to_a.sample.times { desc << Faker::Lorem.sentence(word_count: 10, random_words_to_add: 15) }
 
+  ch = FIRST_CHAR.chars.sample
+  created = Faker::Time.between(from: Time.zone.parse('1997-01-01'), to: Time.zone.parse('2021-05-05'))
+  started = Faker::Time.between(from: Time.zone.parse('1998-01-01'), to: Time.zone.parse('2021-05-05'))
+  started = created if created > started
+
   {
-    title_kana: "さくひん#{n}",
-    title: "作品#{n}",
+    title_kana: "#{ch}さくひん#{n}",
+    title: "#{ch}作品#{n}",
     subtitle_kana: "ふくだい#{n}",
     subtitle: "副題#{n}",
     collection_kana: "さくひんしゅう#{n}",
@@ -24,14 +31,15 @@ books = (1..5000).map do |n|
     first_appearance: "初出#{n}",
     description: desc,
     book_status_id: book_status_id_list.sample,
-    started_on: Time.zone.parse('2021-05-05'),
+    started_on: started,
     copyright_flag: rand(100) <= 90,
+    sortkey: "#{ch}さくひん#{n}",
     user_id: user_id_list.sample,
-    created_at: Time.current,
+    created_at: created,
     updated_at: Time.current
   }
 end
-Book.insert_all(books)
+Book.insert_all(books.sort_by { |b| b[:created_at] })
 
 book_id_list = Book.all.pluck(:id)
 
