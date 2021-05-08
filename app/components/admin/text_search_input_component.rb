0a7bdc6384fa142713f432ba31ec5cf1da07ebd2
takahashim <takahashimm@gmail.com>
2021-05-09 02:24:52 +0900

    add args to Admin::TextSearchInputComponent

--- app/components/admin/text_search_input_component.rb
+++ app/components/admin/text_search_input_component.rb
@@ -3,11 +3,12 @@
 module Admin
   # 作品テキスト検索フォーム用
   class TextSearchInputComponent < ViewComponent::Base
-    def initialize(form:, label:, name:)
+    def initialize(form:, label:, name:, value: nil)
       super
       @form = form
       @label = label
       @name = name
+      @value = value
     end
   end
 end
