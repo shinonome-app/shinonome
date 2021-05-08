e14b0203c4fc4b6002790710077093f417d75b69
takahashim <takahashimm@gmail.com>
2021-05-08 22:19:02 +0900

    add admin books bibclass

--- app/controllers/admin/bibclasses_controller.rb
+++ app/controllers/admin/bibclasses_controller.rb
@@ -2,19 +2,17 @@
 
 module Admin
   class BibclassesController < Admin::ApplicationController
-    before_action :set_site, only: %i[show edit update destroy]
+    before_action :set_bibclass, only: %i[show edit update destroy]
 
-    # GET /admin/bibclasses
     # GET /admin/books/:book_id/bibclasses
     def index
       @bibclasses = Bibclass.all
     end
 
-    # GET /admin/bibclasses/1
+    # GET /admin/books/:book_id/bibclasses/1
     def show
     end
 
-    # GET /admin/bibclasses/new
     # GET /admin/books/:book_id/bibclasses/new
     def new
       @bibclass = Bibclass.new
@@ -23,35 +21,35 @@ module Admin
       end
     end
 
-    # GET /admin/bibclasses/1/edit
     # GET /admin/books/:book_id/bibclasses/1/edit
     def edit
     end
 
-    # POST /admin/bibclasses
+    # POST /admin/books/:book_id/bibclasses
     def create
       @bibclass = Bibclass.new(bibclass_params)
 
       if @bibclass.save
-        redirect_to [:admin, @bibclass], notice: 'Bibclass was successfully created.'
+        redirect_to [:admin, @bibclass.book], notice: 'Bibclass was successfully created.'
       else
         render :new
       end
     end
 
-    # PATCH/PUT /admin/bibclasses/1
+    # PATCH/PUT /admin/books/:book_id/bibclasses/1
     def update
       if @bibclass.update(bibclass_params)
-        redirect_to [:admin, @bibclass], notice: 'Bibclass was successfully updated.'
+        redirect_to [:admin, @bibclass.book], notice: 'Bibclass was successfully updated.'
       else
         render :edit
       end
     end
 
-    # DELETE /bibclasses/1
+    # DELETE /admin/books/:book_id/bibclasses/1
     def destroy
+      book = @bibclass.book
       @bibclass.destroy
-      redirect_to admin_bibclasses_url, notice: 'Bibclass was successfully destroyed.'
+      redirect_to [:admin, book], notice: 'Bibclass was successfully destroyed.'
     end
 
     private
