2bda16e5c982450d3d946c09dfff4ca918f5b945
takahashim <takahashimm@gmail.com>
2021-05-09 02:26:05 +0900

    add scope to worker

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -32,4 +32,30 @@ class Worker < ApplicationRecord
   belongs_to :user, optional: true
   has_many :book_workers, dependent: :destroy
   has_many :books, through: :book_workers
+
+  scope :with_name_kana_search, lambda { |name_kana, selector|
+    case selector.to_i
+    when 1 # を含む
+      where('name_kana like ?', "%#{name_kana}%")
+    when 2 # で始まる
+      where('name_kana like ?', "#{name_kana}%")
+    when 3 # で終わる
+      where('name_kana like ?', "%#{name_kana}")
+    when 4 # と等しい
+      where(name_kana: name_kana)
+    end
+  }
+
+  scope :with_name_search, lambda { |name, selector|
+    case selector.to_i
+    when 1 # を含む
+      where('name like ?', "%#{name}%")
+    when 2 # で始まる
+      where('name like ?', "#{name}%")
+    when 3 # で終わる
+      where('name like ?', "%#{name}")
+    when 4 # と等しい
+      where(name: name)
+    end
+  }
 end
