3d9fc2b6b20089215cd84ec7856bd752bfc5e2f5
takahashim <takahashimm@gmail.com>
2021-05-12 00:22:21 +0900

    fix admin controllers

--- app/controllers/admin/book_workers_controller.rb
+++ app/controllers/admin/book_workers_controller.rb
@@ -1,59 +1,37 @@
 # frozen_string_literal: true
 
-class BookWorkersController < ApplicationController
-  before_action :set_book_worker, only: %i[show edit update destroy]
+module Admin
+  class BookWorkersController < Admin::ApplicationController
+    before_action :set_book_worker, only: %i[destroy]
 
-  # GET /book_workers
-  def index
-    @book_workers = BookWorker.all
-  end
+    # POST /admin/book_workers
+    def create
+      @book_worker = BookWorker.new(book_worker_params)
 
-  # GET /book_workers/1
-  def show; end
-
-  # GET /book_workers/new
-  def new
-    @book_worker = BookWorker.new
-  end
-
-  # GET /book_workers/1/edit
-  def edit; end
-
-  # POST /book_workers
-  def create
-    @book_worker = BookWorker.new(book_worker_params)
-
-    if @book_worker.save
-      redirect_to @book_worker, notice: 'Book worker was successfully created.'
-    else
-      render :new
+      if @book_worker.save
+        redirect_to admin_book_url(@book_worker.book), notice: 'Book worker was successfully created.'
+      else
+        render :new
+      end
     end
-  end
 
-  # PATCH/PUT /book_workers/1
-  def update
-    if @book_worker.update(book_worker_params)
-      redirect_to @book_worker, notice: 'Book worker was successfully updated.'
-    else
-      render :edit
+    # DELETE /admin/book_workers/1
+    def destroy
+      book = @book_worker.boook
+      @book_worker.destroy
+      redirect_to admin_book_url(book), notice: 'Book worker was successfully destroyed.'
     end
-  end
 
-  # DELETE /book_workers/1
-  def destroy
-    @book_worker.destroy
-    redirect_to book_workers_url, notice: 'Book worker was successfully destroyed.'
-  end
+    private
 
-  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_book_worker
+      @book_worker = BookWorker.find(params[:id])
+    end
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_book_worker
-    @book_worker = BookWorker.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
-  def book_worker_params
-    params.require(:book_worker).permit(:book_id, :worker_id, :worker_role_id)
+    # Only allow a list of trusted parameters through.
+    def book_worker_params
+      params.require(:book_worker).permit(:book_id, :worker_id, :worker_role_id)
+    end
   end
 end
--- app/controllers/admin/workers_controller.rb
+++ app/controllers/admin/workers_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Admin
-  class WorkersController < ApplicationController
+  class WorkersController < Admin::ApplicationController
     include Pagy::Backend
     before_action :set_worker, only: %i[show edit update destroy]
 
