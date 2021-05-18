93e519fbb13e20a7d3e0c281fee792ee70d480bf
takahashim <takahashimm@gmail.com>
2021-05-19 01:43:33 +0900

    fix spec

--- spec/requests/admin/exec_commands_spec.rb
+++ spec/requests/admin/exec_commands_spec.rb
@@ -3,9 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Admin::ExecCommands', type: :request do
-  describe 'GET /index' do
+  let(:user) { create(:user) }
+
+  describe 'GET /' do
+    before { sign_in(user) }
+
     it 'returns http success' do
-      get '/admin/exec_commands/index'
+      get '/admin/exec_commands'
       expect(response).to have_http_status(:success)
     end
   end
