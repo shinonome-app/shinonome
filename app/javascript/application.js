e9f10e4160c1cd58e8687d87137bcd5f904f8d83
takahashim <takahashimm@gmail.com>
2021-12-27 17:52:50 +0900

    WIP

---
+++ app/javascript/application.js
@@ -0,0 +1,12 @@
+// Entry point for the build script in your package.json
+
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
+import "stylesheets/application"
