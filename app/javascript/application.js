15adff605914229b12d350167632b6257b97534c
takahashim <takahashimm@gmail.com>
2021-12-27 18:00:59 +0900

    fix errors

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,10 +3,10 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+// import "channels"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-import "stylesheets/application"
+// import "stylesheets/application"
