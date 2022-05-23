2686acce74feebd484297a004135c14a66fbce30
takahashim <takahashimm@gmail.com>
2022-05-23 19:07:43 +0900

    Update config.load_defaults as 7.0

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Shinonome
   # アプリクラス
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
 
     config.autoload_paths << "#{root}/app/lib"
 
