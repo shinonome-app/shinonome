a2470d553f9fa33dbea2125eb02ff787899f3c87
takahashim <takahashimm@gmail.com>
2022-05-26 22:52:24 +0900

    refactor

--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -26,9 +26,12 @@ class ReceiptsController < ApplicationController
   # Only allow a list of trusted parameters through.
   def receipt_params
     params.require(:receipt).permit(:title_kana, :title, :subtitle_kana, :subtitle, :collection_kana,
-                                    :collection, :original_title, :kana_type_id, :first_appearance, :memo, :note, :status, :started_on, :copyright_flag,
+                                    :collection, :original_title, :kana_type_id, :first_appearance,
+                                    :memo, :note, :status, :started_on, :copyright_flag,
                                     :last_name_kana, :last_name, :last_name_en, :first_name_kana, :first_name, :first_name_en, :person_note,
-                                    :worker_kana, :worker_name, :email, :url, :original_book_title, :publisher, :first_pubdate, :input_edition, :original_book_title2, :publisher2, :first_pubdate2,
+                                    :worker_kana, :worker_name, :email, :url,
+                                    :original_book_title, :publisher, :first_pubdate, :input_edition,
+                                    :original_book_title2, :publisher2, :first_pubdate2,
                                     :person_id, :worker_id, :register_status, :original_book_note)
   end
 end
