9331209a79cd56840ae3544e5c140ab607db5ce7
takahashim <takahashimm@gmail.com>
2022-07-12 16:59:31 +0900

    WIP

--- app/models/proofread.rb
+++ app/models/proofread.rb
@@ -35,19 +35,31 @@
 #  fk_rails_...  (work_id => works.id)
 #
 
+# 校正受付
 class Proofread < ApplicationRecord
   belongs_to :work
   belongs_to :workfile, optional: true
   belongs_to :worker, optional: true
 
-  enum assign_status: { not_assigned: 0, assigned: 1 }
-  enum order_status: { not_ordered: 0, ordered: 1 }
+  enum assign_status: { non_assigned: 0, assigned: 1 }
+  enum order_status: { non_ordered: 0, ordered: 1 }
+  enum work_copy: { need_copy: 0, no_need_copy: 1 }
+  enum work_print: { need_print: 0, no_need_print: 1 }
 
   scope :active, -> { where(deleted_at: nil) }
+
+  before_validation :set_statuses
 
   validates :worker_id, numericality: { only_integer: true }, allow_nil: true
   validates :worker_name, presence: true
   validates :worker_kana, presence: true
   validates :email, presence: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  private
+
+  def set_statuses
+    self.assign_status ||= 0
+    self.order_status ||= 0
+  end
 end
