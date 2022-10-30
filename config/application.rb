e93142afd5e7c0629b3251b587db7776aa7b3073
takahashim <takahashimm@gmail.com>
2022-10-31 03:03:24 +0900

    Use envvar MAIN_SITE_URL

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,7 @@ module Shinonome
 
     # Modify error form class to support TailwindCSS
     config.action_view.field_error_proc = proc { |html_tag, _instance| content_tag :div, html_tag, class: 'field_with_errors p-1 bg-red-300 table' }
+
+    config.x.main_site_url = ENV['MAIN_SITE_URL']
   end
 end
