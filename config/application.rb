c80344477991708d2a0de962f7ab22c433d8772d
takahashim <takahashimm@gmail.com>
2022-10-31 03:06:06 +0900

    rubocop

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,6 @@ module Shinonome
     # Modify error form class to support TailwindCSS
     config.action_view.field_error_proc = proc { |html_tag, _instance| content_tag :div, html_tag, class: 'field_with_errors p-1 bg-red-300 table' }
 
-    config.x.main_site_url = ENV['MAIN_SITE_URL']
+    config.x.main_site_url = ENV.fetch('MAIN_SITE_URL', nil)
   end
 end
