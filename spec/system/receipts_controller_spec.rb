b025f0988353dbdf2c21132194783da4cd212d5d
takahashim <takahashimm@gmail.com>
2023-01-01 19:16:39 +0900

    rspec: add a spec for receipt registration

--- spec/system/receipts_controller_spec.rb
+++ spec/system/receipts_controller_spec.rb
@@ -50,6 +50,7 @@ describe ReceiptsController do
       choose 'receipt_form_sub_works_attributes_0_copyright_flag_0'
 
       click_button('確認')
+      expect(page).to have_current_path receipts_previews_path, ignore_query: true
 
       expect(page).to have_content('入力受付システム：記入事項の確認')
       expect(page).to have_content('青空太郎')
@@ -57,6 +58,56 @@ describe ReceiptsController do
       expect(page).to have_content('「赤い鳥」1918（大正7）年7月')
       expect(page).to have_content('作品についてサンプル')
       expect(page).to have_content('旧字旧仮名')
+    end
+
+    it '一通り入力して確認→登録すると登録される' do
+      visit '/receipts/new'
+      fill_in '工作員読み', with: 'あおぞらたろう'
+      fill_in '工作員名', with: '青空太郎'
+      fill_in 'e-mail', with: 'test@example.com'
+      fill_in 'ホームページ', with: 'http://sample.example.com'
+      fill_in 'receipt_form_last_name_kana', with: 'あくたがわ'
+      fill_in 'receipt_form_last_name', with: '芥川'
+      fill_in 'receipt_form_first_name_kana', with: 'りゅうのすけ'
+      fill_in 'receipt_form_first_name', with: '竜之介'
+      fill_in 'receipt_form_person_note', with: '備考テスト'
+      fill_in '底本名', with: '芥川龍之介全集２'
+      fill_in 'receipt_form_publisher', with: 'ちくま文庫、筑摩書房'
+      fill_in 'receipt_form_first_pubdate', with: '1986（昭和61）年10月28日'
+      fill_in 'receipt_form_input_edition', with: '1996（平成8）年7月15日第11刷'
+      fill_in 'receipt_form_original_book_note', with: '底本の備考'
+      fill_in '底本の親本名', with: '筑摩全集類聚版芥川龍之介全集'
+      fill_in '底本の親本出版社名', with: '筑摩書房'
+      fill_in '底本の親本初版発行年', with: '1971（昭和46）年3月～11月に刊行'
+      fill_in '作品名読み', with: 'くものいと'
+      fill_in 'receipt_form_sub_works_attributes_0_title', with: '蜘蛛の糸'
+      fill_in 'receipt_form_sub_works_attributes_0_subtitle_kana', with: 'ふくだいさんぷる'
+      fill_in 'receipt_form_sub_works_attributes_0_subtitle', with: '副題サンプル'
+      fill_in '原題', with: '原題サンプル'
+      select '旧字旧仮名', from: 'receipt_form_sub_works_attributes_0_kana_type_id'
+      fill_in '初出', with: '「赤い鳥」1918（大正7）年7月'
+      fill_in '作品について', with: '作品についてサンプル'
+      fill_in 'receipt_form_sub_works_attributes_0_note', with: '作品備考サンプル'
+      choose 'receipt_form_sub_works_attributes_0_copyright_flag_0'
+
+      click_button('確認')
+      click_button('登録')
+
+      expect(page).to have_current_path receipts_thanks_path
+
+      expect(page).to have_content('入力受付システム：受付完了')
+      expect(page).to have_content('入力をお申し込みいただき、ありがとうございました。')
+
+      receipt = Receipt.last
+      expect(receipt.last_name).to eq '芥川'
+      expect(receipt.last_name_kana).to eq 'あくたがわ'
+      expect(receipt.first_name).to eq '竜之介'
+      expect(receipt.first_name_kana).to eq 'りゅうのすけ'
+      expect(receipt.worker_kana).to eq 'あおぞらたろう'
+      expect(receipt.worker_name).to eq '青空太郎'
+      expect(receipt.url).to eq 'http://sample.example.com'
+      expect(receipt.publisher).to eq 'ちくま文庫、筑摩書房'
+      expect(receipt.publisher2).to eq '筑摩書房'
     end
   end
 end
