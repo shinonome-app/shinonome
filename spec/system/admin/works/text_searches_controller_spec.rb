094aa37277066718ad4c92e4c424e5af83b85c68
takahashim <takahashimm@gmail.com>
2023-01-01 20:02:47 +0900

    rspec: add specs for Admin::Works::TextSearchesController

---
+++ spec/system/admin/works/text_searches_controller_spec.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe Admin::Works::TextSearchesController do
+  let(:user) { create(:user) }
+
+  before { sign_in(user) }
+
+  describe '#index' do
+    it '作品名で絞り込みされる' do
+      create(:work, title: '蜘蛛の糸')
+      create(:work, title: '作品その2')
+
+      visit '/admin/works'
+
+      fill_in '作品名:', with: '蛛の'
+
+      page.find('h2', text: '作品テキスト検索').sibling('form[action="/admin/works/text_searches"]').click_button('検索')
+
+      expect(page).to have_content('作品検索結果一覧')
+      expect(page).to have_content('蜘蛛の糸')
+      expect(page).not_to have_content('作品その2')
+    end
+
+    it '「で始まる」を使うと作品名の先頭で絞り込みされる' do
+      create(:work, title: '蜘蛛の糸')
+      create(:work, title: '黒後家蜘蛛の会')
+
+      visit '/admin/works'
+
+      fill_in '作品名:', with: '蜘蛛'
+      select 'で始まる', from: 'text_selector_title'
+
+      page.find('h2', text: '作品テキスト検索').sibling('form[action="/admin/works/text_searches"]').click_button('検索')
+
+      expect(page).to have_content('作品検索結果一覧')
+      expect(page).to have_content('蜘蛛の糸')
+      expect(page).not_to have_content('黒後家蜘蛛の会')
+    end
+
+    it '「と等しい」を使うと作品名の完全一致で絞り込みされる' do
+      create(:work, title: '蜘蛛の糸')
+      create(:work, title: '蜘蛛')
+
+      visit '/admin/works'
+
+      fill_in '作品名:', with: '蜘蛛'
+      select 'と等しい', from: 'text_selector_title'
+
+      page.find('h2', text: '作品テキスト検索').sibling('form[action="/admin/works/text_searches"]').click_button('検索')
+
+      expect(page).to have_content('作品検索結果一覧')
+      expect(page).to have_content('蜘蛛')
+      expect(page).not_to have_content('蜘蛛の糸')
+    end
+  end
+end
