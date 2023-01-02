9189527c55da437ca97ed39e1690d1699ff72444
takahashim <takahashimm@gmail.com>
2023-01-02 16:28:09 +0900

    fix proofread and its specs

--- spec/system/proofreads_controller_spec.rb
+++ spec/system/proofreads_controller_spec.rb
@@ -50,6 +50,89 @@ describe ProofreadsController do
         expect(page).not_to have_content('taro@example.com')
         expect(page).not_to have_content('https://example.com/taro')
       end
+
+      it '要コピーを選んで送付先を入力しないとエラーになる' do
+        visit "/proofreads/people/#{person.id}"
+
+        find('form[action="/proofreads/new"]').first('input[type="checkbox"]').set(true)
+        click_button('確認')
+
+        expect(page).to have_content('校正受付システム：必要事項の記入')
+
+        fill_in '連絡事項', with: '連絡事項1'
+        fill_in '工作員ID', with: worker.id
+        check('要コピー')
+
+        click_button('確認')
+
+        expect(page).to have_content('校正受付システム：必要事項の記入')
+        expect(page).to have_content('1件のエラーが見つかりました')
+        expect(page).to have_content('送付先を入力してくだい')
+      end
+
+      it '確認画面から登録に進むと登録してメールを飛ばす' do
+        allow(UserMailer).to receive(:register_proofread).once.and_call_original
+
+        visit "/proofreads/people/#{person.id}"
+
+        find('form[action="/proofreads/new"]').first('input[type="checkbox"]').set(true)
+        click_button('確認')
+
+        expect(page).to have_content('校正受付システム：必要事項の記入')
+
+        fill_in '連絡事項', with: '連絡事項1'
+        fill_in '工作員ID', with: worker.id
+
+        click_button('確認')
+
+        expect(page).to have_content('校正受付システム：記入事項の確認')
+        click_button('登録')
+
+        expect(page).to have_content('校正受付システム：受付完了')
+        expect(UserMailer).to have_received(:register_proofread)
+
+        proofread = Proofread.last
+        expect(proofread.worker_id).to eq worker.id
+        expect(proofread.worker_name).to eq worker.name
+        expect(proofread.worker_kana).to eq worker.name_kana
+        expect(proofread.email).to eq worker.worker_secret.email
+        expect(proofread.need_print?).to be false
+        expect(proofread.need_copy?).to be false
+        expect(proofread.ordered?).to be false
+        expect(proofread.assigned?).to be false
+      end
+
+      it 'もろもろ入力して確認画面から登録に進むとそれらが登録される' do
+        visit "/proofreads/people/#{person.id}"
+
+        find('form[action="/proofreads/new"]').first('input[type="checkbox"]').set(true)
+        click_button('確認')
+
+        expect(page).to have_content('校正受付システム：必要事項の記入')
+
+        fill_in '連絡事項', with: '連絡事項1'
+        fill_in '工作員ID', with: worker.id
+        fill_in '送付先', with: "送付先A\n送付先B\n送付先C\n"
+        check('要コピー')
+        check('要プリントアウト')
+
+        click_button('確認')
+
+        expect(page).to have_content('校正受付システム：記入事項の確認')
+        click_button('登録')
+
+        expect(page).to have_content('校正受付システム：受付完了')
+        proofread = Proofread.last
+        expect(proofread.worker_id).to eq worker.id
+        expect(proofread.worker_name).to eq worker.name
+        expect(proofread.worker_kana).to eq worker.name_kana
+        expect(proofread.email).to eq worker.worker_secret.email
+        expect(proofread.address).to eq "送付先A\r\n送付先B\r\n送付先C\r\n"
+        expect(proofread.need_print?).to be true
+        expect(proofread.need_copy?).to be true
+        expect(proofread.ordered?).to be false
+        expect(proofread.assigned?).to be false
+      end
     end
 
     context '入力フォームで工作員の名前を入力する場合' do
@@ -98,6 +181,41 @@ describe ProofreadsController do
         expect(page).to have_content('連絡事項1')
         expect(page).to have_content('user1@example.com')
         expect(page).to have_content('https://example.com/user1')
+      end
+
+      it '確認画面から登録に進むと登録してメールを飛ばす' do
+        allow(UserMailer).to receive(:register_proofread).once.and_call_original
+
+        visit "/proofreads/people/#{person.id}"
+
+        find('form[action="/proofreads/new"]').first('input[type="checkbox"]').set(true)
+        click_button('確認')
+
+        expect(page).to have_content('校正受付システム：必要事項の記入')
+
+        fill_in '連絡事項', with: '連絡事項1'
+        fill_in '工作員読み', with: 'こうさくいんいち'
+        fill_in '工作員名', with: '工作員1'
+        fill_in 'e-mail', with: 'user1@example.com'
+        fill_in 'ホームページ', with: 'https://example.com/user1'
+
+        click_button('確認')
+
+        expect(page).to have_content('校正受付システム：記入事項の確認')
+        click_button('登録')
+
+        expect(page).to have_content('校正受付システム：受付完了')
+        expect(UserMailer).to have_received(:register_proofread)
+
+        proofread = Proofread.last
+        expect(proofread.worker_id).to be_nil
+        expect(proofread.worker_name).to eq '工作員1'
+        expect(proofread.worker_kana).to eq 'こうさくいんいち'
+        expect(proofread.email).to eq 'user1@example.com'
+        expect(proofread.need_print?).to be false
+        expect(proofread.need_copy?).to be false
+        expect(proofread.ordered?).to be false
+        expect(proofread.assigned?).to be false
       end
     end
   end
