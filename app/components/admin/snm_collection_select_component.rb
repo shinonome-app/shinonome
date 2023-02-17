d575108631dba55c8fd782609c0f2c97a634e2af
takahashim <takahashimm@gmail.com>
2023-02-17 19:27:58 +0900

    rubocop

--- app/components/admin/snm_collection_select_component.rb
+++ app/components/admin/snm_collection_select_component.rb
@@ -14,7 +14,7 @@ module Admin
       @options = options || { prompt: '選択してください' }
 
       default_class = {
-        class: "w-full min-w-[100px] bg-dropdownIcon bg-no-repeat bg-[right_0.8rem_center] rounded px-2 py-2 border border-ab_form focus:outline-none focus:border-ab_focus focus:ring-1 focus:ring-ab_focus focus:shadow-[0px_1px_13px_0px_#bee3f8] cursor-pointer appearance-none"
+        class: 'w-full min-w-[100px] bg-dropdownIcon bg-no-repeat bg-[right_0.8rem_center] rounded px-2 py-2 border border-ab_form focus:outline-none focus:border-ab_focus focus:ring-1 focus:ring-ab_focus focus:shadow-[0px_1px_13px_0px_#bee3f8] cursor-pointer appearance-none'
       }
       @html_options = html_options ? default_class.merge(html_options) : default_class
 
