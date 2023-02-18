8b80bb92a75fe16c0f723697b541ea56bfa2897e
takahashim <takahashimm@gmail.com>
2023-02-18 15:12:45 +0900

    Fix for long titles

--- app/components/admin/works_table_component.rb
+++ app/components/admin/works_table_component.rb
@@ -12,17 +12,15 @@ module Admin
     end
 
     def before_render
-      @header = %w[作品ID 作品名 副題 著者 翻訳者 仮名遣い 状態 状態の開始日]
+      @header = %w[作品ID 作品名 副題 著者/翻訳者 仮名遣い 状態/状態の開始日]
       @body = @works.map do |work|
         [
           work.id,
           link_to(work.title, admin_work_path(work)),
           work.subtitle,
-          work.author_text,
-          work.translator_text,
+          safe_join([work.author_text, tag.br, work.translator_text]),
           work.kana_type.name,
-          tag.snm_status(statusType: work.work_status.color, label: work.work_status.name),
-          work.started_on
+          safe_join([tag.snm_status(statusType: work.work_status.color, label: work.work_status.name), tag.br, work.started_on])
         ]
       end
     end
