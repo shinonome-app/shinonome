89555dffaa19476f43e34d1884d59d3f1bbf38e7
takahashim <takahashimm@gmail.com>
2023-12-14 22:38:44 +0900

    Fix mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,9 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "shinonome_production"
 
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
