94ab344c7be54716ab7ada9b56912418678f582a
takahashim <takahashimm@gmail.com>
2023-12-16 18:35:08 +0900

    Use admin_path in sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@
 class ApplicationController < ActionController::Base
   private
 
+  def after_sign_in_path_for(resource_or_scope)
+    admin_path
+  end
+
   def after_sign_out_path_for(resource_or_scope)
     admin_path
   end
