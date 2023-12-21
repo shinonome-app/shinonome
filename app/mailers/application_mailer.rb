ae27b33b59eea1f0b1290955c7f9b574b6d76a15
takahashim <takahashimm@gmail.com>
2023-12-21 17:43:00 +0900

    Fix from email address

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base # rubocop:disable Style/Documentation
-  default from: 'from@example.com'
+  default from: Rails.application.config.x.reception_email || 'from@example.com'
   layout 'mailer'
 end
