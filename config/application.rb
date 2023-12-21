948e30c101d5314b67f381b27912a5f647c2795b
takahashim <takahashimm@gmail.com>
2023-12-21 17:42:30 +0900

    Fix unescaped Unicode string in ENV

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,21 @@ Bundler.require(*Rails.groups)
 module Shinonome
   # アプリクラス
   class Application < Rails::Application
+    # ENVの文字列を変換する
+    class EnvConverter
+      def self.unescape_ustring(str)
+        new.unescape_ustring(str)
+      end
+
+      def unescape_ustring(str)
+        str.gsub(/\\u([0-9A-Fa-f]{4})/) { unescape_uchar(::Regexp.last_match(1)) }
+      end
+
+      def unescape_uchar(str)
+        [str.to_i(16)].pack('U*')
+      end
+    end
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
@@ -29,7 +44,7 @@ module Shinonome
     # config.action_view.field_error_proc = proc { |html_tag, _instance| content_tag :div, html_tag, class: 'p-1 bg-red-300 table' }
 
     config.x.main_site_url = ENV.fetch('MAIN_SITE_URL', nil)
-    config.x.site_name = ENV.fetch('SITE_NAME', 'Shinonome')
+    config.x.site_name = EnvConverter.unescape_ustring(ENV.fetch('SITE_NAME', 'Shinonome'))
     config.x.csv_dir = ENV.fetch('CSV_DIR', Rails.public_path.join('csv'))
     config.x.reception_email = ENV.fetch('RECEPTION_EMAIL', nil)
   end
