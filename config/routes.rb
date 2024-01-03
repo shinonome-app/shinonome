331ec70fbea0fe2c18ba744f9d9c3a3e3645f195
takahashim <takahashimm@gmail.com>
2024-01-03 14:09:40 +0900

    Add routes from komadome, to preview

--- config/routes.rb
+++ config/routes.rb
@@ -145,5 +145,40 @@ Rails.application.routes.draw do
     # resources :worker_roles
   end
 
+  ## for previews
+  resources :index_pages, only: [] do
+    collection do
+      get 'index_top'
+      get 'index_all'
+
+      ## People
+      get 'person_:id', to: 'people#index', constraints: { id: /[kstnhmyrw]?a|zz/ }, as: :people
+      get 'person:id', to: 'people#show', constraints: { id: /\d+/ }, as: :person
+
+      get 'person_all_:id', to: 'index_pages#person_all_index', constraints: { id: /[kstnhmyrw]?a|zz/ }
+
+      get 'person_inp_:id', to: 'index_pages#person_inp_index', constraints: { id: /[kstnhmyrw]?a|zz/ }
+
+      get 'person_all', to: 'index_pages#person_all'
+
+      get 'list_inp:id_page', to: 'index_pages#list_inp_show', constraints: { id_page: /\d+_\d+/ }, as: :list_inp
+
+      get 'sakuhin_:id_page', to: 'index_pages#work_index', constraints: { id_page: /([kstnhmyrw]?[aiueo]|zz|nn)\d+/ }, as: :sakuhin
+
+      get 'sakuhin_inp_:id_page', to: 'index_pages#work_inp_index', constraints: { id_page: /([kstnhmyrw]?[aiueo]|zz|nn)\d+/ }, as: :sakuhin_inp
+
+      # Whatsnew
+      get 'whatsnew:page', to: 'whatsnews#index', constraints: { page: /\d+/ }, as: :whatsnew
+      get 'whatsnew_:year_page', to: 'whatsnews#index_year', constraints: { year_page: /\d\d\d\d_\d+/ }, as: :whatsnew_year
+
+      # Download zip files
+      get 'list_person_all', to: 'downloads#list_person_all', constraints: { format: 'zip' }
+      get 'list_person_all_extended', to: 'downloads#list_person_all_extended', constraints: { format: 'zip' }
+      get 'list_person_all_utf8', to: 'downloads#list_person_all_utf8', constraints: { format: 'zip' }
+      get 'list_person_all_extended_utf8', to: 'downloads#list_person_all_extended_utf8', constraints: { format: 'zip' }
+    end
+  end
+
+
   mount LetterOpenerWeb::Engine, at: '/letter_opener'
 end
