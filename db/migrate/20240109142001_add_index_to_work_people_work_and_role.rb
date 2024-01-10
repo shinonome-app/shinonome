2b2fb9483396e7880e1a54ca3e3a1956589dac0b
takahashim <takahashimm@gmail.com>
2024-01-10 20:31:03 +0900

    rubocop

--- db/migrate/20240109142001_add_index_to_work_people_work_and_role.rb
+++ db/migrate/20240109142001_add_index_to_work_people_work_and_role.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class AddIndexToWorkPeopleWorkAndRole < ActiveRecord::Migration[7.1]
   def change
-    add_index :work_people, [:work_id, :role_id]
+    add_index :work_people, %i[work_id role_id]
   end
 end
