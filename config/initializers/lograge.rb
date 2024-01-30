9376642100db0aa9b77c1b43246b38f7875de1ad
takahashim <takahashimm@gmail.com>
2024-01-30 23:05:25 +0900

    do not use lograge in development

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -3,10 +3,12 @@
 require 'lograge/sql/extension'
 
 Rails.application.configure do
-  config.lograge.enabled = true if Rails.env.production?
-  config.lograge.formatter = Lograge::Formatters::Json.new
-  # config.lograge.keep_original_rails_log = true
-  # config.lograge.logger = ActiveSupport::Logger.new(Rails.root.join('log', "lograge-#{Rails.env}.log"))
+  if Rails.env.production?
+    config.lograge.enabled = true
+    config.lograge.formatter = Lograge::Formatters::Json.new
+    # config.lograge.keep_original_rails_log = true
+    # config.lograge.logger = ActiveSupport::Logger.new(Rails.root.join('log', "lograge-#{Rails.env}.log"))
 
-  config.lograge.ignore_actions = ['HealthController#show']
+    config.lograge.ignore_actions = ['HealthController#show']
+  end
 end
