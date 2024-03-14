427d779b86f3d7a994ca1cc55b26a9c6a46ccf39
takahashim <takahashimm@gmail.com>
2024-03-15 03:31:14 +0900

    Fix component

--- app/components/admin/snm_text_area_component.rb
+++ app/components/admin/snm_text_area_component.rb
@@ -3,15 +3,16 @@
 module Admin
   # text_area用コンポーネント
   class SnmTextAreaComponent < ViewComponent::Base
-    def initialize(form:, name:, label: nil, rows: nil, errors: nil)
+    def initialize(form:, name:, label: nil, rows: nil, errors: nil, error_key: nil)
       super
       @form = form
       @name = name
       @label = label || form.object.class.human_attribute_name(name)
       @rows = rows || 5
       @errors = errors || {}
+      @error_key = error_key || name
     end
 
-    attr_reader :form, :name, :label, :rows, :errors
+    attr_reader :form, :name, :label, :rows, :errors, :error_key
   end
 end
