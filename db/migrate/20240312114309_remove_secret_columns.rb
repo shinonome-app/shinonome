87be0df9308c740acd7bb06fa0fd066d9e7900f5
takahashim <takahashimm@gmail.com>
2024-03-15 00:09:47 +0900

    Fix migration file

--- db/migrate/20240312114309_remove_secret_columns.rb
+++ db/migrate/20240312114309_remove_secret_columns.rb
@@ -2,19 +2,66 @@
 
 class RemoveSecretColumns < ActiveRecord::Migration[7.1]
   def change
-    remove_column :sites, :email, :text
-    remove_column :sites, :owner_name, :text
-    remove_column :sites, :note, :text
+    reversible do |direction|
+      change_table :sites, bulk: true do |t|
+        direction.up do
+          t.remove :email
+          t.remove :owner_name
+          t.remove :note
+        end
 
-    remove_column :original_books, :note, :text
+        direction.down do
+          t.text :email
+          t.text :owner_name
+          t.text :note
+        end
+      end
 
-    remove_column :people, :note_user_id, :bigint
-    remove_column :people, :note, :text
+      change_table :original_books, bulk: true do |t|
+        direction.up do
+          t.remove :note
+        end
 
-    remove_column :works, :orig_text, :text
+        direction.down do
+          t.text :note
+        end
+      end
 
-    remove_column :workfiles, :opened_on, :date
-    remove_column :workfiles, :user_id, :bigint
-    remove_column :workfiles, :note, :text
+      change_table :people, bulk: true do |t|
+        direction.up do
+          t.remove :note_user_id
+          t.remove :note
+        end
+
+        direction.down do
+          t.bigint :note_user_id
+          t.text :note
+        end
+      end
+
+      change_table :works, bulk: true do |t|
+        direction.up do
+          t.remove :orig_text
+        end
+
+        direction.down do
+          t.text :orig_text
+        end
+      end
+
+      change_table :workfiles, bulk: true do |t|
+        direction.up do
+          t.remove :opened_on
+          t.remove :user_id
+          t.remove :note
+        end
+
+        direction.down do
+          t.date :opened_on
+          t.bigint :user_id
+          t.text :note
+        end
+      end
+    end
   end
 end
