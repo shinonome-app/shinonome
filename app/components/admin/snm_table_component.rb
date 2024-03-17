2d3f2d0396208706e45c1ce57a76bd08f2623416
takahashim <takahashimm@gmail.com>
2024-03-18 01:05:17 +0900

    add classes attr for Admin::SnmTableComponent

--- app/components/admin/snm_table_component.rb
+++ app/components/admin/snm_table_component.rb
@@ -3,10 +3,13 @@
 module Admin
   # 縦表用コンポーネント
   class SnmTableComponent < ViewComponent::Base
-    def initialize(header:, body:)
+    DEFAULT_TD_CLASS = 'text-center py-0 px-5'
+
+    def initialize(header:, body:, classes: nil)
       super
       @header = header
       @body = body
+      @classes = classes || ([DEFAULT_TD_CLASS] * @body.size)
     end
   end
 end
