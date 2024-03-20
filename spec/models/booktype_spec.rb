f161a7160c59d7834830d7fc9b240e2a6d4ea431
takahashim <takahashimm@gmail.com>
2024-03-21 02:34:57 +0900

    add a spec

--- spec/models/booktype_spec.rb
+++ spec/models/booktype_spec.rb
@@ -13,5 +13,10 @@
 require 'rails_helper'
 
 RSpec.describe Booktype do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'seed' do
+    it '正しいseedを持つ' do
+      expect(Booktype.find(1).name).to eq '底本'
+      expect(Booktype.find(2).name).to eq '底本の親本'
+    end
+  end
 end
