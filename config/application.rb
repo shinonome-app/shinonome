9d4fb3f8c1782a0e9104926415a3c93315016f92
takahashim <takahashimm@gmail.com>
2025-01-01 20:12:10 +0900

    update load_defaults to 7.2

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module Shinonome
     end
 
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 7.2
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
