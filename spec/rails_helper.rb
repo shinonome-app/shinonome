415c88d687ca7087771e663d6042a79694202bc1
takahashim <takahashimm@gmail.com>
2025-01-03 00:53:33 +0900

    allow to use ActiveJob::TestHelper in any type of specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -72,7 +72,7 @@ RSpec.configure do |config|
   config.include RequestSpecHelper, type: :request
   config.include ViewComponent::TestHelpers, type: :component
   config.include Capybara::RSpecMatchers, type: :component
-  config.include ActiveJob::TestHelper, type: :system
+  config.include ActiveJob::TestHelper
 
   config.before(:suite) do
     Rails.application.load_seed # loading seeds
