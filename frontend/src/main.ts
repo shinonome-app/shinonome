b463b087ae9be334e5948872c1a698c2ed8bef40
takahashim <takahashimm@gmail.com>
2025-01-13 21:08:31 +0900

    remove snm-message-bar

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,4 +3,3 @@ export { SnmTable } from "./components/snm-table";
 export { SnmTableVertical } from "./components/snm-table-vertical";
 export { SnmTable2column } from "./components/snm-table-2column";
 export { SnmStatus } from "./components/snm-status";
-export { SnmMessageBar } from "./components/snm-message-bar";
