79f6c972feddf817a3de8e40e6aa8b6925d8a66f
takahashim <takahashimm@gmail.com>
2025-01-13 17:21:28 +0900

    fix: add Admin::Works::UnknownCreatorSearchesController#index

--- app/controllers/admin/works/unknown_creator_searches_controller.rb
+++ app/controllers/admin/works/unknown_creator_searches_controller.rb
@@ -3,7 +3,12 @@
 module Admin
   module Works
     class UnknownCreatorSearchesController < ApplicationController
-      def index; end
+      include Pagy::Backend
+
+      # GET /admin/works/unknown_creator_searches
+      def index
+        @pagy, @works = pagy(Work.without_role_one_work_people.order(created_at: :desc), limit: 50)
+      end
     end
   end
 end
