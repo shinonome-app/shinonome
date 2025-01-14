e0f8f8150072f2bdab559876df7107889253d48f
takahashim <takahashimm@gmail.com>
2025-01-14 11:27:39 +0900

    frontend: remove all component

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,1 @@
 // Components
-export { SnmTable } from "./components/snm-table";
-export { SnmTableVertical } from "./components/snm-table-vertical";
-export { SnmTable2column } from "./components/snm-table-2column";
