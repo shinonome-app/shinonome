61b821b9a8a12c062904d7af972ffae32ac202b4
takahashim <takahashimm@gmail.com>
2025-01-15 13:17:59 +0900

    use tailwindcss plugin for puma

--- config/puma.rb
+++ config/puma.rb
@@ -31,6 +31,9 @@ port ENV.fetch('PORT', 3000)
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
 
+# use TailwindCSS
+plugin :tailwindcss if ENV.fetch("RAILS_ENV", "development") == "development"
+
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
 pidfile ENV['PIDFILE'] if ENV['PIDFILE']
