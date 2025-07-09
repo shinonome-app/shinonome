0c72517de30ac0246b51c349976eb6f484c1ecac
takahashim <takahashimm@gmail.com>
2025-07-09 11:36:49 +0900

    feat: add safe_html helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,20 @@ module ApplicationHelper
     str&.gsub(/\r?\n/, "<br>\n")
   end
 
+  # HTMLを安全に出力するヘルパー
+  def safe_html(text)
+    return '' if text.blank?
+
+    sanitize(text,
+             tags: %w[
+               a abbr b blockquote br caption cite code col colgroup dd div dl dt em h1 h2 h3 h4 h5 h6 hr i img
+               li ol p pre q small span strong sub sup table tbody td tfoot th thead tr u ul
+             ],
+             attributes: %w[
+               href src alt title width height class id name colspan rowspan align valign
+             ])
+  end
+
   # for admin pages
   def snm_link(label, href: nil, font_color: 'sidebar', icon: nil, button_style: 'primary', target: nil)
     render(Admin::SnmLinkComponent.new(label:, href:, font_color:, icon:, button_style:, target:))
