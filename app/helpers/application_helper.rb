d6385e40a9f1b0dd88c2f1938201ebc245d63a10
takahashim <takahashimm@gmail.com>
2025-07-09 12:26:20 +0900

    feat: add safe_url? and safe_link_url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,6 +19,33 @@ module ApplicationHelper
              ])
   end
 
+  # URLが安全かどうかをチェックするヘルパー
+  def safe_url?(url)
+    return false if url.blank?
+
+    # URLの妥当性をチェック
+    uri = URI.parse(url)
+
+    # 許可されたスキームのみ（http, https, mailto）
+    allowed_schemes = %w[http https mailto]
+    allowed_schemes.include?(uri.scheme&.downcase)
+  rescue URI::InvalidURIError
+    # 不正なURLの場合はfalseを返す
+    false
+  end
+
+  # link_to(url, url)形式専用の安全なリンク生成ヘルパー
+  def safe_link_url(url, html_options = {})
+    return '-' if url.blank?
+
+    if safe_url?(url)
+      link_to(url, url, html_options)
+    else
+      # 危険なURLの場合はリンクではなく文字列として表示
+      url
+    end
+  end
+
   # for admin pages
   def snm_link(label, href: nil, font_color: 'sidebar', icon: nil, button_style: 'primary', target: nil)
     render(Admin::SnmLinkComponent.new(label:, href:, font_color:, icon:, button_style:, target:))
