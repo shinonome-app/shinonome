c487a1bd1b30a3055ff32d0fdcf457cf5f7f7f45
takahashim <takahashimm@gmail.com>
2026-06-21 19:52:44 +0900

    fix: support tailwind CDN for preview of admin/people#show

--- app/controllers/admin/people/previews_controller.rb
+++ app/controllers/admin/people/previews_controller.rb
@@ -5,11 +5,24 @@ module Admin
     class PreviewsController < ApplicationController
       layout false
 
+      # use tailwind CDN for preview
+      PREVIEW_TAILWIND_CDN = '<script src="https://cdn.tailwindcss.com"></script>'
+
       def show
         person = Person.find(params[:id])
         context = NatsuzoraContext::PersonBuilder.new(person).build
         html = NatsuzoraRenderer.new.render('people/show.ntzr', context)
-        render html: html.html_safe # rubocop:disable Rails/OutputSafety
+        render html: with_preview_styles(html).html_safe # rubocop:disable Rails/OutputSafety
+      end
+
+      private
+
+      def with_preview_styles(html)
+        if html.include?('</head>')
+          html.sub('</head>', "#{PREVIEW_TAILWIND_CDN}\n</head>")
+        else
+          "#{PREVIEW_TAILWIND_CDN}\n#{html}"
+        end
       end
     end
   end
